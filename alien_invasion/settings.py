dda6dbd8a83fc0ceede30f5717b768995b5eb4b4
A. Sacman <sacmail08@gmail.com>
2020-11-12 17:10:26 -0500

    Adding Alien Invasion game to git hub

---
+++ alien_invasion/settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """A class to store settings for game."""
+    
+    def __init__(self):
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.bg_color = (255, 255, 255)
+        
+        #Ship Settings
+        self.ship_speed = 1.5
